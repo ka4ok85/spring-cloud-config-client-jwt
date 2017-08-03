7fd72a26dfcaead692c9e909eeca97dd9a0a7055
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-08-02 20:52:56 -0400

    added Token REST Model

---
+++ src/main/java/com/github/ka4ok85/config/client/jwt/models/Token.java
@@ -0,0 +1,27 @@
+package com.github.ka4ok85.config.client.jwt.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Token {
+
+	@JsonProperty
+	private String token;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	@Override
+	public String toString() {
+		return "Token [token=" + token + "]";
+	}
+
+}
