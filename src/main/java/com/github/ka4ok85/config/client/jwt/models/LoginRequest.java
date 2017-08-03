706f90c69f687d0194d419b3f026ed4ddaf686e7
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-08-02 20:53:22 -0400

    added LoginRequest REST Model

---
+++ src/main/java/com/github/ka4ok85/config/client/jwt/models/LoginRequest.java
@@ -0,0 +1,38 @@
+package com.github.ka4ok85.config.client.jwt.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class LoginRequest {
+
+	@JsonProperty
+	private String username;
+
+	@JsonProperty
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginRequest [username=" + username + ", password=" + password + "]";
+	}
+
+}
